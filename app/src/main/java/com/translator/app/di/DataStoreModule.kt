640368b904a946387c04357bced7664d5b58455d
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-05-10 18:34:38 +0200

    Update file

--- app/src/main/java/com/translator/app/di/DataStoreModule.kt
+++ app/src/main/java/com/translator/app/di/DataStoreModule.kt
@@ -3,7 +3,6 @@ package com.translator.app.di
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.core.DataStoreFactory
-import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
 import androidx.datastore.dataStoreFile
 import com.translator.app.data.settings.AppSettings
 import com.translator.app.data.settings.AppSettingsSerializer
@@ -12,25 +11,20 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DataStoreModule {
-
     @Provides
     @Singleton
     fun provideAppSettingsDataStore(
         @ApplicationContext context: Context,
         serializer: AppSettingsSerializer
-    ): DataStore<AppSettings> =
-        DataStoreFactory.create(
+    ): DataStore<AppSettings> {
+        return DataStoreFactory.create(
             serializer = serializer,
-            produceFile = { context.dataStoreFile("app_settings_encrypted.json") },
-            corruptionHandler = ReplaceFileCorruptionHandler { AppSettings() },
-            scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+            produceFile = { context.dataStoreFile("translator_settings.json") }
         )
+    }
 }
