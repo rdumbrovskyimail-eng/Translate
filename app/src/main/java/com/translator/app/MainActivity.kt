35404d1d13fd54aabcabd9a0ad537d506d1f5123
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-05-11 23:42:39 +0200

    Update file

--- app/src/main/java/com/translator/app/MainActivity.kt
+++ app/src/main/java/com/translator/app/MainActivity.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.translator.app.presentation.navigation.AppNavGraph
+import com.translator.app.presentation.settings.SettingsViewModel
 import com.translator.app.presentation.theme.GeminiLiveTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,7 +18,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
-            GeminiLiveTheme {
+            val settingsVm: SettingsViewModel = hiltViewModel()
+            val settings by settingsVm.settings.collectAsStateWithLifecycle()
+            val themeId = com.translator.app.presentation.theme.AppThemeId.fromName(settings.themeId)
+            GeminiLiveTheme(themeId = themeId) {
                 AppNavGraph()
             }
         }
