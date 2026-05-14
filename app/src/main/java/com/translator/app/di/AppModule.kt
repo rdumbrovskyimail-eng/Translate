15ab3bf144bb78cc0c8d962becc3ce6088af3838
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-05-14 18:00:05 +0200

    Update file

--- app/src/main/java/com/translator/app/di/AppModule.kt
+++ app/src/main/java/com/translator/app/di/AppModule.kt
@@ -14,6 +14,7 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
     @Provides
     @Singleton
     fun provideLiveClient(logger: AppLogger): LiveClient = GeminiLiveClient(logger)
